ddeb82f6f8904f79f9fe132eaae2cb4c3823135e
chongxuanliang <clia0005@student.monash.edu>
2020-05-27 11:26:08 +1000

    Added some headers(There are too many files :( )

--- Ansible/launch-instance.sh
+++ Ansible/launch-instance.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+# Team 36: Is income and education influence sentiment?
+# city: Melbourne Chongxuan Liang - 1118236    
+# city: Melbourne Ruitong Liu - 947759
+# city: Melbourne Yuhan Zhao  - 1042775
+# city: Beijing Zengxu Zhao  - 978084
+# city: Melbourne Zhiqi Liu  - 1071551
 
 . ./unimelb-comp90024-2020-grp-36-openrc.sh; ansible-playbook --ask-become-pass launch-instance.yaml
